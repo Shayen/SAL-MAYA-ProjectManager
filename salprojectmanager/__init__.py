b6f7127044d6ad75dc440729b29d15c9651ddde7
Sirasit sawetprom <sirasit1234@gmail.com>
2017-05-20 02:42:50 +0700

    Update __init__.py

--- salprojectmanager/__init__.py
+++ salprojectmanager/__init__.py
@@ -4,5 +4,3 @@ from salprojectmanager.SALWin import loadProjectwin
 from salprojectmanager.salCore import core
 from salprojectmanager.salCore import ENV_PATH
 from salprojectmanager.salCore import XML_mod
-
-global __version__   
